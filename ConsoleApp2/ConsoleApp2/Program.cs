5ba49f1cd5b8d4959af8a72cedf5a9aa0d5be5dd
resqa <resul1965qurbanov@gmail.com>
2022-11-04 00:23:12 +0400

    home task 2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int a = 20, b = 680, c = a + b;
+if (a > 50 && b > 50)
+{
+    Console.WriteLine("toplananlar 50den boyukdur ve cem " + c + "berabardir");
+}
+
+else
+{
+    Console.WriteLine("bu ededler uygun deyil");
+}
